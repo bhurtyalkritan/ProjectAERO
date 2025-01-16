3bcb3bafdbff79feb4609f88d7aaf625a0cbd7d5
Kritan Bhurtyal <kbhurtya@unc.edu>
2025-01-16 13:37:09 -0500

    fixes

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,7 @@
 import pytest
 import httpx
 
-BASE_URL = "http://127.0.0.1:8000"  # Ensure your FastAPI app is running during the tests
+BASE_URL = "http://127.0.0.1:8000"
 
 @pytest.fixture(scope="module")
 def httpx_client():
